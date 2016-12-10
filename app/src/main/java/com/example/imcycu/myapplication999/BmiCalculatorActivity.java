b929a0b31d33e9032c633d616a4e502d3c9c4452
qq850405 <pig850405@gmail.com>
2016-12-10 13:17:22 +0800

    layout finished

---
+++ app/src/main/java/com/example/imcycu/myapplication999/BmiCalculatorActivity.java
@@ -0,0 +1,13 @@
+package com.example.imcycu.myapplication999;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BmiCalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bmi_calculator);
+    }
+}
